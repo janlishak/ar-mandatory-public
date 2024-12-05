75d6219a84da563e4ba4da4e9df5a9345515b450
Gino Franco Fazzi <gifa@itu.dk>
2024-12-05 16:07:33 +0100

    Robot w IR

--- robot2.py
+++ robot2.py
@@ -119,6 +119,8 @@ class ThymioController:
                         node.v.motor.left.target = self.motor_values[0]
                         node.v.motor.right.target = self.motor_values[1]
                         node.v.leds.top = self.led_values
+                        node.v.leds.top.left = self.led_values
+                        node.v.leds.top.right = self.led_values
                         node.v.leds.bottom.left = self.led_values
                         node.v.leds.bottom.right = self.led_values
                         # Pass the sensors to the robot
